3f30e7a95eedcbe7e0ed6d60d28636fe6e629740
Pedro Llerenas <pdllerenas@gmail.com>
2024-10-21 20:14:07 +0000

    solution

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
-
+df = pd.read_csv('medical_examination.csv')
 # 2
-df['overweight'] = None
+df['overweight'] = np.where((df['weight']/((df['height']/100)**2)) > 25, 1, 0)
 
 # 3
-
+df.loc[df['cholesterol'] == 1, 'cholesterol'] = 0 
+df.loc[df['cholesterol'] > 1, 'cholesterol']= 1
+df.loc[df['gluc'] == 1, 'gluc'] = 0
+df.loc[df['gluc'] > 1, 'gluc'] = 1
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars = ['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'], value_name='value', var_name='variable')
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby('cardio', as_index=False).value_counts()
+    df_cat = df_cat.rename(columns={'count': 'total'})
 
     # 7
-
+    df_cat = df_cat.astype({'cardio': 'float', 'value': 'float', 'total': 'float'})
 
 
     # 8
-    fig = None
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 10))
+    fig = sns.catplot(data=df_cat, kind="bar", x="variable", y="total", hue="value", col="cardio", order=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke']).figure
 
 
     # 9
@@ -38,21 +40,24 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df.loc[
+        (df['ap_lo'] <= df['ap_hi']) & 
+        (df['height'] >= df['height'].quantile(0.025)) & 
+        (df['height'] <= df['height'].quantile(0.975)) & 
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))] 
 
     # 12
-    corr = None
-
+    corr = df_heat.corr()
+    print(corr)
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots()
 
     # 15
-
+    fig = sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', linewidths=.5, ax=ax).figure
 
 
     # 16
